033b2743b30fa7e2754bf2f26db37c58f87dbf1b
Damion Wiltshire <drw.5452@gmail.com>
2020-08-09 09:25:45 -0700

    Added root and get user by id route

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+
+const userRouter = require('./routes/user-router')
+
+app.use(methodOverride('_method'))
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
+
+app.get('/', (req, res) => {
+    res.json(
+        {
+            message: 'ok',
+            data: 'Show index view here'
+    })
+})
+
+app.use('/user', userRouter);
